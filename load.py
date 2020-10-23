d684f26539317760890037ddb61cac93aee7cd27
Jasper Wong <jasperwong@jaspers-mbp.lan>
2020-10-22 21:51:15 -0700

    add header import files

--- load.py
+++ load.py
@@ -1,3 +1,10 @@
+from google.cloud import datastore
+from flask import Flask, request
+import json
+import constants
+
+client = datastore.Client()
+
 # Python Blueprint template that creates Blueprint named 'boat', 2nd par __name__ the blueprint
 # know where it's defined, url_prefix will prepend to all URLs associated with the blueprint.
 bp = Blueprint('load', __name__, url_prefix='/loads')
