7a83d6295580f812b7f02647a1c9bc8c42f13eb5
Jasper Wong <jasperwong@jaspers-mbp.lan>
2020-11-10 22:19:16 -0800

    I'm tired, lazy to clean up

--- client_info.py
+++ client_info.py
@@ -3,5 +3,5 @@
 CLIENT_ID = '823710541896-sq5est11004asi1qbu3d5ehc7954hnrq.apps.googleusercontent.com'
 CLIENT_SECRET = 'odswOXW_inMxoJnXV_pZxLzp'  # Read from a file or environmental variable in a real app
 SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
-REDIRECT_URI = 'http://localhost:8080/oauth' # for when running on local, comment out when using GAE
-# REDIRECT_URI = 'https://hw06-wongjasp.wl.r.appspot.com/oauth' # for running on GAE, comment when using local
+# REDIRECT_URI = 'http://localhost:8080/oauth' # for when running on local, comment out when using GAE
+REDIRECT_URI = 'https://hw06-wongjasp.wl.r.appspot.com/oauth' # for running on GAE, comment when using local
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
+'''
+Name:   Jasper Wong
+Date:   11-10-2020
+Source: CS 493 W02-W05 HW, Google Cloud Platform docs and examples
 
+'''
+import json
 import flask
 from flask import render_template
 import requests
@@ -7,68 +12,70 @@ import requests
 # for client info constants
 import client_info
 
-# for state generator string, create random string
-import random
-import string
-
 app = flask.Flask(__name__)
 
 # when launched to GAE, comment out for local (creates unique user id)
 import uuid
 app.secret_key = str(uuid.uuid4())
 
-# add state generator (random generator pynative.com).
-# Not needed anymore, going to use uuid module v4 for random id as state var
-# def get_random_string(length):
-#     letters_and_digits = string.ascii_letters + string.digits
-#     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
-#     # print("Random alphanumeric String is:", result_str)
-#     return result_str
-
-
 @app.route('/')
 def index():
+  return render_template('index.html')
+
+
+# @app.route('/')
+# def index():
+@app.route('/cred')
+def index_cred():
   if 'credentials' not in flask.session:
-    print("1. No credentials, getting it!") # <-----
+    # print("1. No credentials, getting it!") # <-----
     return flask.redirect(flask.url_for('oauth'))
   credentials = json.loads(flask.session['credentials'])
   if credentials['expires_in'] <= 0:
     return flask.redirect(flask.url_for('oauth'))
   else:
-    print("8. Back in index after getting access token from POST") # <----
+    # print("8. Back in index after getting access token from POST") # <----
     headers = {'Authorization': 'Bearer {}'.format(credentials['access_token'])}
-    # get credentials since GAE log doesn't show local
-    # print("access_token: " + credentials['access_token'])
     req_uri = 'https://people.googleapis.com/v1/people/me?personFields=names'
-    print("9. perform get request and assigning content to r var") # <-----
+    # print("9. perform get request and assigning content to r var") # <----- GET REQUEST
     r = requests.get(req_uri, headers=headers)
-    # return r.text
-    # print(get_random_string(10))
-    return render_template('index2.html', user_info = r.text)
+
+    res_test = json.loads(r.text) # JSON string to Python dict
+
+    print(res_test["names"][0]["familyName"])
+    print(res_test["names"][0]["givenName"].split(' ', 1)[0])
+    familyName = res_test["names"][0]["familyName"]
+    givenName = res_test["names"][0]["givenName"].split(' ', 1)[0]
+
+    # flask.session["state"] = str(uuid.uuid4()) # comment out when deploying to GCP, only use for testing local
+    return render_template('user-info.html', familyName = familyName, givenName = givenName, stateVal = flask.session["state"])
 
 
 @app.route('/oauth')
 def oauth():
   if 'code' not in flask.request.args:
-    print("2. In /oauth, No code in flask request arguments, initial redirect to get code to be able to get access token") # <----
-    print("2-1. Generate random alpha and num for state") # <---
-    # flask.session['state'] = get_random_string(10) # gen random string for state track
+    # print("2. In /oauth, No code in flask request arguments, initial redirect to get code to be able to get access token") # <----
+    # print("2-1. Generate random alpha and num for state") # <---
     flask.session['state'] = str(uuid.uuid4()) # gen random string for state track
-    # print(flask.session['state']) # yes you can just call session object
-    # print(str(uuid.uuid4()))
-    # print(str(uuid.uuid4()))
-    # auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
-    #             '&client_id={}&redirect_uri={}&scope={}').format(client_info.CLIENT_ID, client_info.REDIRECT_URI, client_info.SCOPE)
+    print(flask.session['state']) # yes you can just call session object
     auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
                 '&client_id={}&redirect_uri={}&scope={}&state={}').format(client_info.CLIENT_ID, client_info.REDIRECT_URI, client_info.SCOPE, flask.session['state'])
-    print("3. In /oauth, About to send initial redirect") # <--
-    print(auth_uri)
-    return flask.redirect(auth_uri)
+    # print("3. In /oauth, About to send initial redirect") # <--
+    return flask.redirect(auth_uri) # <------- takes me to page to choose account
   else:
-    print("4. In /oauth, After initial redirect to get auth code, onward to POST to server to get access token")
-    # state_info = flask.request.args.get("state")
-    print(flask.request.args.get("state"))
-    # print("state info: " + str(state_info))
+    # print("4. Arrived here after account choosen, In /oauth, After initial redirect to get auth code, onward to POST to server to get access token")
+    state_info = flask.request.args.get("state")
+    # print(type(state_info))
+    print(flask.session['state']) # <--- does not work locally, will break. Only works on GCP.
+   
+    # Ensure that the request is not a forgery and that the user sending, 
+    # this connect request is the expected user. source: google doc OpenID Connect
+    # comment out when working locally since session does not persist for some reason
+    if state_info != flask.session['state']:
+      response = make_response(json.dumps('Invalid state parameter.'), 401)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
     auth_code = flask.request.args.get('code')
     
     data = {'code': auth_code,
@@ -80,12 +87,12 @@ def oauth():
     # have to comment out r = ... when following oauth 2.0 demo and postman exercise
     # r = ... is POST to get access token. When using log to get the token, invalid_grant error because the token
     # has been used. that is my hypothesis.
-    r = requests.post('https://oauth2.googleapis.com/token', data=data) # test <------------
-    print("5. In /oauth, sent post with code authorization to get access token")
+    r = requests.post('https://oauth2.googleapis.com/token', data=data) # test <------------ POST portion of demo to get access token
+    # print("5. In /oauth, sent post with code authorization to get access token")
     flask.session['credentials'] = r.text
-    print("6. In /oauth, added context of response from post to credentials with sessions")
+    # print("6. In /oauth, added context of response from post to credentials with sessions")
     print("7. redirecting to / endpoint")
-    return flask.redirect(flask.url_for('index'))
+    return flask.redirect(flask.url_for('index_cred'))
 
 
 if __name__ == '__main__':
