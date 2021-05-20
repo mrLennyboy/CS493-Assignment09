6dd6b97d76b6b6dd7de871b4db24eaae7fdeb840
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-05-20 16:22:21 -0700

    Update client_info.py

--- client_info.py
+++ client_info.py
@@ -1,7 +1,7 @@
 # constants for client secret info and redirects 
 # constants for hw09
 CLIENT_ID = '153129745149-jn0an4tjr2t7nslud3qm51th2e5087ma.apps.googleusercontent.com'
-CLIENT_SECRET = 'YgnCx3xxywKGPN7hjPF9Km1A'
+CLIENT_SECRET = #
 
 # REDIRECT_URI = 'http://localhost:8080/oauth' # for when running on local, comment out when using GAE
 REDIRECT_URI = 'https://hw09-wongjasp.wl.r.appspot.com/oauth' # for running on GAE, comment when using local
